7d3adfa8c2ecb4ce4feb4abacf6c7978697d542e
PratikPatel50 <71339745+PratikPatel50@users.noreply.github.com>
2022-02-09 23:33:41 +0530

    init

--- calc.js
+++ calc.js
@@ -1,52 +1,21 @@
-function add(a, b) {
-    console.log("addition =", a+b)//we will use ,c )
+function add(a,b){
+    console.log(a+b);
 }
 
-function sub(a, b) {
-    console.log("substraction =", a-b)
+function sub(x,y){
+    console.log(x-y);
 }
 
-function mul(a, b) {
-    console.log("Multiplication =", a*b)
-}
-
-function div(a, b) {
-    console.log("Division =", a/b)
-}
-
-module.exports.addition = add
-module.exports.substraction = sub
-module.exports.Multiplication = mul
-module.exports.Divition = div
-
-//export
-
-/*
-function add(a, b) {
-    var c = a + b
-    console.log("addition =", c)//we will use ,c )
-}
-
-function sub(x, y) {
-    var z = x - y
-    console.log("substraction =", +z)
-}
-
-function mul(x, y) {
-    var z = x * y
-    console.log("Multiplication =", +z)
+function mul(x,y){
+    console.log(x*y);
 }
 
 function div(x, y) {
-    var z = x / y
-    console.log("Divition =", +z)
+    console.log(x / y);
 }
-*/
-/*
-export
-/*
-add(30, 20)
-sub(10, 20)
-mul(10, 20)
-div(10, 20)
-*/
+module.exports.addition = add
+module.exports.substraction = sub
+module.exports.multiplication = mul
+module.exports.divison = div
+//export
+ 
--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const calc = require("./calc") //import
+const calc = require("./calc") //import 
+ 
 
-calc.addition(12, 12);
-calc.substraction(16, 10);
-calc.Multiplication(16, 10);
-calc.Divition(16, 10);
-
-
-
+calc.addition(12,12)
+calc.substraction(12, 12)
+calc.multiplication(12, 12)
+calc.divison(12, 12)
 
 
 
@@ -18,18 +16,24 @@ calc.Divition(16, 10);
 
 
 
+// console.log("hello")
 
-/*
-var x = 10 -- > global variable
-let y = 20 -- > local variable
-const z = 30 --> always constant variable 
-*/
-/*
-console.log("hello")
+// var x =10 //--> global variable  
+// let y =20 //--> local variable 
+// const z = 30 // --> constant variable 
 
-a = 10
-console.log(a)
 
-a="abcdefg"
-console.log(a)
-*/  
+
+// a=10//number 
+// console.log(a)
+
+// a="royal"//string 
+// console.log(a)
+
+// function add(a,b){
+//    var c=a+b
+//     console.log("addition = ",c); 
+// }
+
+// add(10,20) 
+
