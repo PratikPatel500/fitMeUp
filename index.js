c59c9ed044cec2d2b1a9d318018923e127f3412b
PratikPatel50 <71339745+PratikPatel50@users.noreply.github.com>
2022-02-09 22:04:33 +0530

    init

---
+++ index.js
@@ -0,0 +1,35 @@
+const calc = require("./calc") //import
+
+calc.addition(12, 12);
+calc.substraction(16, 10);
+calc.Multiplication(16, 10);
+calc.Divition(16, 10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var x = 10 -- > global variable
+let y = 20 -- > local variable
+const z = 30 --> always constant variable 
+*/
+/*
+console.log("hello")
+
+a = 10
+console.log(a)
+
+a="abcdefg"
+console.log(a)
+*/  
