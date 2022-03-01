7bf1a96cab50b716e11798a9ead542fb35869eb4
PratikPatel50 <71339745+PratikPatel50@users.noreply.github.com>
2022-03-01 22:11:09 +0530

    user

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -26,16 +26,32 @@ module.exports.addUser=function(req,res){
 }
 
 
-// //list
-// module.exports.addUser=function(req,res){
-    
-// }
+//list
+
+module.exports.getAllUsers = function(req,res){
+
+    UserModel.find().populate("role").exec(function(err,data){
+        if(err){
+            res.json({msg:"Something went wrong!!!",data:err,status:-1})
+        }else{
+            res.json({msg:"Users Return....",data:data,status:200})
+        }   
+    })
+}
 
 
-// //delete
-// module.exports.addUser=function(req,res){
-    
-// }
+//delete
+module.exports.deleteUser = function(req,res){
+    //params userid
+    let userId = req.params.userId //postman---> userId
+    UserModel.deleteOne({_id:userId},function(err,data){
+        if(err){
+                res.json({msg:"Something went wrong!!!",data:err,status:-1})
+            }else{
+                res.json({msg:"user Removed....",data:data,status:200})
+            }
+        })
+}
 
 
 // //update
--- model/user-model.js
+++ model/user-model.js
@@ -1,15 +1,15 @@
 const mongoose=require("mongoose")
  
 const UserSchema= new mongoose.Schema({
-    FirstName : {
+    firstName : {
         type:String,
         required:true
     },
     Email : {
-        type:"String"
+        type:String
     },
     password : {
-        type:"String"
+        type:String
     },
     role : {
             type:mongoose.Schema.Types.ObjectId,
@@ -17,6 +17,6 @@ const UserSchema= new mongoose.Schema({
     }
 })
 
-const UserModel =mongoose.model("user",UserSchema)
+const UserModel = mongoose.model("user",UserSchema)
 module.exports = UserModel
 
